393471e5f833b1ec80d6c9646442fb7bb89e15bf
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-11-30 12:37:20 +0530

    first commit

---
+++ src/InputProcessor.java
@@ -0,0 +1,5 @@
+public class InputProcessor {
+    public static void main(String[] args) {
+        System.out.println("0th of arg is  : "+args[0]);
+    }
+}
---
+++ src/Scanner.java
@@ -0,0 +1,6 @@
+/**
+ * Created by gauravkhuperkar on 11/30/16.
+ */
+public class Scanner {
+
+}
