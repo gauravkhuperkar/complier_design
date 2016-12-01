8d33996f0a313a452c2c1e70f36bd96e50c88ccc
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2016-12-01 22:30:11 +0530

    Using fileReader and bufferedReader for reading file.

--- src/InputProcessor.java
+++ src/InputProcessor.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InputProcessor {
-    public static void main(String[] args) {
-        System.out.println("0th of arg is  : "+args[0]);
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            FileReader fileReader = new FileReader(args[0]);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while(bufferedReader.readLine() != null) {
+                System.out.println(bufferedReader.readLine());
+            }
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 }
